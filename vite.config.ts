8b138df5b8bf2c20fa56e1773d437049da7f8aa9
Jérôme Abel <contact@jeromeabel.net>
2023-11-28 12:46:53 +0100

    fix: base site

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // site: 'https://jeromeabel.github.io',
-  base: '/kungfuschool',
+  base: '/neptunebeerclub',
   test: {
     globals: true,
     environment: 'happy-dom',
