7155cf63c9f631763a067f6b79a0552c444748de
Jérôme Abel <contact@jeromeabel.net>
2023-11-28 12:46:33 +0100

    setup: fix base site

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // site: 'https://jeromeabel.github.io',
+  base: '/kungfuschool',
   test: {
     globals: true,
     environment: 'happy-dom',
