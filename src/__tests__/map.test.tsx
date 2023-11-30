7b6e6c2dcae952633ce9897fe3e5f67320b9088c
Jérôme Abel <contact@jeromeabel.net>
2023-11-30 17:14:26 +0100

    fix: test map in BarProvider

--- src/__tests__/map.test.tsx
+++ src/__tests__/map.test.tsx
@@ -3,11 +3,15 @@ import { render } from '@testing-library/react'; // screen
 //import userEvent from '@testing-library/user-event';
 
 // To Test
-import { BarMap } from '@features/bar';
+import { BarMap, BarProvider } from '@features/bar';
 
 // Tests
 describe('Unit tests on Map Feature', () => {
-  it('Should render the Map Component correctly', () => {
-    render(<BarMap />);
+  it('Should render the Map Component correctly inside BarProvider', () => {
+    render(
+      <BarProvider>
+        <BarMap />
+      </BarProvider>,
+    );
   });
 });
