bf7b3a6475c680432deec930300c876f2fcac734
Jérôme Abel <contact@jeromeabel.net>
2023-12-01 10:24:31 +0100

    feat: import online and offline data with errors catching

--- src/features/bar/bar-service.ts
+++ src/features/bar/bar-service.ts
@@ -1,14 +1,25 @@
 import { ApiResponse } from './types';
 
 const API_URL = import.meta.env.VITE_API_BARS_URL as string;
+const LOCAL_JSON = '../../assets/bars.json';
 
 export const fetchBars = async () => {
   try {
     const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch bars from online API. Status: ${response.status}`);
+    }
     const data = (await response.json()) as ApiResponse;
     return data.data;
-  } catch (error) {
-    console.error('Error fetching bars:', error);
-    throw error;
+  } catch (onlineError) {
+    console.error('Error fetching bars online. Trying locally...', onlineError);
+    try {
+      // Dynamically import DATA only if the online fetch fails
+      const { data: localData } = (await import(/* @vite-ignore */ LOCAL_JSON)) as ApiResponse;
+      return localData;
+    } catch (localError) {
+      console.error('Error fetching bars locally:', localError);
+      throw new Error('Failed to fetch bars both online and locally.');
+    }
   }
 };
